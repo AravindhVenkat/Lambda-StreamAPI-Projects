37d11da32ad928851969f39f7284d2a794a6df6d
Aravindh V <90633974+AravindhVenkat@users.noreply.github.com>
2021-12-19 00:15:11 +0530

    Update CitizenshipApp.java

--- src/Citizenship/CitizenshipApp.java
+++ src/Citizenship/CitizenshipApp.java
@@ -7,17 +7,17 @@ public class CitizenshipApp {
 	public static void main(String[] args) {
 		
 		//passing null in Category
-		Citizenship.addPersonAndSetCategory(new Person(101,"Aravi",22,null));
-		Citizenship.addPersonAndSetCategory(new Person(121,"Bala",46,null));
-		Citizenship.addPersonAndSetCategory(new Person(165,"Deepak",76,null));
-		Citizenship.addPersonAndSetCategory(new Person(501,"Ajay",34,null));
+		CitizenshipService.addPersonAndSetCategory(new Person(101,"Aravi",22,null));
+		CitizenshipService.addPersonAndSetCategory(new Person(121,"Bala",46,null));
+		CitizenshipService.addPersonAndSetCategory(new Person(165,"Deepak",76,null));
+		CitizenshipService.addPersonAndSetCategory(new Person(501,"Ajay",34,null));
 		
-		ArrayList<Person> al =new ArrayList<>(Citizenship.hm.values());
+		ArrayList<Person> al =new ArrayList<>(CitizenshipService.hm.values());
 		
-		System.out.println("ID deleted?---->"+Citizenship.deletePersonById(121)+"\n");	//boolean
-		System.out.println("Citizen List of given category: "+Citizenship.showPersonDetailsByCategory("Young")+"\n"); //List of Citizen with specified Category
-		System.out.println("Number of Citizen aged above 35: "+Citizenship.countPersonAbove35(al)+"\n");
-		System.out.println("Citizen List sorted by Age Ascending: "+Citizenship.sortPersonOnAgeAsc(al)+"\n");
-		System.out.println("Citizen List sorted by Age Descending: "+Citizenship.sortPersonOnAgeDesc(al));
+		System.out.println("ID deleted?---->"+CitizenshipService.deletePersonById(121)+"\n");	//boolean
+		System.out.println("Citizen List of given category: "+CitizenshipService.showPersonDetailsByCategory("Young")+"\n"); //List of Citizen with specified Category
+		System.out.println("Number of Citizen aged above 35: "+CitizenshipService.countPersonAbove35(al)+"\n");
+		System.out.println("Citizen List sorted by Age Ascending: "+CitizenshipService.sortPersonOnAgeAsc(al)+"\n");
+		System.out.println("Citizen List sorted by Age Descending: "+CitizenshipService.sortPersonOnAgeDesc(al));
 	}
 }
